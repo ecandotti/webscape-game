3cb859541f39cd2051ca4a15ab3f5868c55f7b48
spideai <spideai@outlook.fr>
2021-02-01 10:36:44 +0100

    changed app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Projet React Pour le challenge 48h
         </p>
         <a
           className="App-link"
