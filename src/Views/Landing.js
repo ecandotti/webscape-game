f1588dd617b4afbdfa4d2642e8fcff0349cb93cf
Ke0x <quentin.mayer@ynov.com>
2021-02-01 17:51:14 +0100

    Page Login

--- src/Views/Landing.js
+++ src/Views/Landing.js
@@ -13,7 +13,6 @@ export default class Landing extends React.Component{
     render() {
         const { setViewId } = this.props;
         return (
-<<<<<<< HEAD
                 <div className="App">
                     <div className="overlap">
                         <div className="title">
@@ -25,17 +24,10 @@ export default class Landing extends React.Component{
                     </div>
                     
                     <div>
-                        <button onClick={() => setViewId(1)}> Commencer </button>
+                        <button onClick={() => setViewId(2)}> Commencer </button>
                     </div>
                 </div>
             
-=======
-            <div className="App">
-                <p>Titre</p>
-
-                <button onClick={() => setViewId(1)}> continuer </button>
-            </div>
->>>>>>> 56f9f8b52f7f6c29793d1b3b3031a579dd0149ce
         )
     }
 }
