348f7212759ec08fd5a4576834d504cc0c53ba70
RGUERARD <robin.guerard@ynov.com>
2021-02-01 19:10:38 +0100

    landing page finie

--- src/Views/Landing.js
+++ src/Views/Landing.js
@@ -13,39 +13,22 @@ export default class Landing extends React.Component{
     render() {
         const { setViewId } = this.props;
         return (
-<<<<<<< HEAD
-                <div className="App">
-                    <div className="overlap">
-                        <div className="title">
-                            <p>Escape Game</p>
-                        </div>
-                        <div className="intro">
-                            <p></p>
-                        </div>  
-=======
             <div className="App">
                 <div className="overlap">
                     <div className="title">
-                        <p>ESCAPE GAME : LES GALERES DE JEAN-MICHEL</p>
+                        <h2>ESCAPE GAME : LES GALERES DE JEAN-MICHEL</h2>
                     </div>
                     <div className="intro">
-                        <p>Bonjour et bienvenue sur notre projet de groupe réalisé dans le cadre du Challenge 48h.</p>
-                        <p>Le principe de celui-ci est de résoudre plusieurs énigmes qui permettront à notre personnage principal :
-                            Jean-Michel, de résoudre les différents problèmes auxquels il fait face sur son lieu de travail.
-                        </p>
-                        <p>Le principe est simple, vous devrez résoudre plusieurs mini-jeux / énigmes afin de pouvoir avancer dans l’histoire et aider Jean-Michel à pouvoir travailler sans embûches.</p>
-                        <p>Appuyez sur le bouton ‘Commencer’ pour débuter la partie.</p>
->>>>>>> d9526123a5e04d07e696e168b9630ca0fbd275e5
+                        <p>Bonjour et bienvenue sur notre projet de groupe réalisé dans le cadre du Challenge 48h.<br/>
+                        Le principe de celui-ci est de résoudre plusieurs énigmes qui permettront à notre personnage principal : <br/>
+                        Jean-Michel, de résoudre les différents problèmes auxquels il fait face sur son lieu de travail.<br/><br/>
+
+                        Le principe est simple, vous devrez résoudre plusieurs mini-jeux / énigmes afin de pouvoir avancer dans l’histoire et aider Jean-Michel à pouvoir travailler sans embûches.<br/><br/>
+                        Appuyez sur le bouton ‘Commencer’ pour débuter la partie.</p>
                     </div>
-                    <div>
-                        <button onClick={() => setViewId(2)}> Commencer </button>
-                    </div>
+                    <button className="start" onClick={() => setViewId(2)}> Commencer </button>
                 </div>
-<<<<<<< HEAD
-            
-=======
             </div>
->>>>>>> d9526123a5e04d07e696e168b9630ca0fbd275e5
         )
     }
 }
