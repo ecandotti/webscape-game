b25ade3c3d6dceccdcc17aabddad8f2205fc7efa
spideai <spideai@outlook.fr>
2021-02-01 14:43:17 +0100

    basic navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './assets/logo.svg';
+import React from "react"
 import './styles/App.css';
+import Landing from "./Views/Landing";
+import StepOne from "./Views/StepOne";
+import StepTwo from "./Views/StepTwo";
+import StepThree from "./Views/StepThree";
+import StepFour from "./Views/StepFour";
+import StepFive from "./Views/StepFive";
+import StepSix from "./Views/StepSix";
+import StepSeven from "./Views/StepSeven";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Projet React Pour le challenge 48h
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// View Id => 0 = landing,
+
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewId: 0
+    }
+  }
+
+  setViewId = (viewId) => {
+    this.setState({viewId})
+  };
+
+
+  render() {
+    const { viewId } = this.state;
+
+    switch (viewId) {
+      case 0:
+        return (
+            <Landing setViewId={this.setViewId} />
+        );
+      case 1:
+        return (
+            <StepOne setViewId={this.setViewId} />
+        );
+      case 2:
+        return (
+            <StepTwo setViewId={this.setViewId} />
+        );
+      case 3:
+        return (
+            <StepThree setViewId={this.setViewId} />
+        );
+      case 4:
+        return (
+            <StepFour setViewId={this.setViewId} />
+        );
+      case 5:
+        return (
+            <StepFive setViewId={this.setViewId} />
+        );
+      case 6:
+        return (
+            <StepSix setViewId={this.setViewId} />
+        );
+      case 7:
+        return (
+            <StepSeven setViewId={this.setViewId} />
+        );
+
+      default:
+        return (
+            <div>
+              broken nav
+            </div>
+        )
+    }
+  }
 }
-
-export default App;
