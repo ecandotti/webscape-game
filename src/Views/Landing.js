41c92ef3863d5d072c28dc0d81f4ce01d6ee0973
spideai <spideai@outlook.fr>
2021-02-01 18:05:44 +0100

    workingVersion

--- src/Views/Landing.js
+++ src/Views/Landing.js
@@ -13,39 +13,35 @@ export default class Landing extends React.Component{
     render() {
         const { setViewId } = this.props;
         return (
-<<<<<<< HEAD
-                <div className="App">
-                    <div className="overlap">
-                        <div className="title">
-                            <p>Escape Game</p>
-                        </div>
-                        <div className="intro">
-                            <p></p>
-                        </div>  
-=======
             <div className="App">
                 <div className="overlap">
                     <div className="title">
-                        <p>ESCAPE GAME : LES GALERES DE JEAN-MICHEL</p>
+                        <p>
+                            ESCAPE GAME : LES GALERES DE JEAN-MICHEL
+                        </p>
                     </div>
                     <div className="intro">
-                        <p>Bonjour et bienvenue sur notre projet de groupe réalisé dans le cadre du Challenge 48h.</p>
-                        <p>Le principe de celui-ci est de résoudre plusieurs énigmes qui permettront à notre personnage principal :
+                        <p>
+                            Bonjour et bienvenue sur notre projet de groupe réalisé dans le cadre du Challenge 48h.
+                        </p>
+                        <p>
+                            Le principe de celui-ci est de résoudre plusieurs énigmes qui permettront à notre personnage principal :
                             Jean-Michel, de résoudre les différents problèmes auxquels il fait face sur son lieu de travail.
                         </p>
-                        <p>Le principe est simple, vous devrez résoudre plusieurs mini-jeux / énigmes afin de pouvoir avancer dans l’histoire et aider Jean-Michel à pouvoir travailler sans embûches.</p>
-                        <p>Appuyez sur le bouton ‘Commencer’ pour débuter la partie.</p>
->>>>>>> d9526123a5e04d07e696e168b9630ca0fbd275e5
+                        <p>
+                            Le principe est simple, vous devrez résoudre plusieurs mini-jeux / énigmes afin de pouvoir avancer dans l’histoire et aider Jean-Michel à pouvoir travailler sans embûches.
+                        </p>
+                        <p>
+                            Appuyez sur le bouton ‘Commencer’ pour débuter la partie.
+                        </p>
                     </div>
                     <div>
-                        <button onClick={() => setViewId(2)}> Commencer </button>
+                        <button onClick={() => setViewId(2)}>
+                            Commencer
+                        </button>
                     </div>
                 </div>
-<<<<<<< HEAD
-            
-=======
-            </div>
->>>>>>> d9526123a5e04d07e696e168b9630ca0fbd275e5
+           </div>
         )
     }
 }
--- src/Views/StepTwo.js
+++ src/Views/StepTwo.js
@@ -2,51 +2,49 @@ import React from "react"
 import '../styles/App.css';
 import '../styles/StepTwo.css'
 
-export default class StepTwo extends React.Component{
+export default class StepTwo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            state: "state",
             email: "",
-            motdepasse: ""
-        }
-        this.handleChangeEmail = this.handleChangeEmail.bind(this);
-        this.handleChangeMdp = this.handleChangeMdp.bind(this);
-
-        this.handleSubmit = this.handleSubmit.bind(this);
+            password: ""
+        };
     }
 
-    handleChangeEmail(event) {
+    handleChangeEmail = (event) => {
         this.setState({email: event.target.value});
-        console.log(this.state.email)
-    }
+    };
 
-    handleChangeMdp(event) {
-        this.setState({motdepasse: event.target.value});
-        console.log(this.state.motdepasse)
-    }
+    handleChangeMdp = (event) => {
+        this.setState({password: event.target.value});
+    };
 
-    handleSubmit(event) {
-        alert('Login');
-      }
+    handleSubmit = () => {
+        console.log('Login');
+    };
 
     render() {
         const { setViewId } = this.props;
+        const { email, password } = this.state;
         return (
             <div className="App">
                 <div className="loginBox">
                     <p>Login</p>
                     <div className="inputLogin">
                         <label className="inputText">Email</label>
-                        <input className="inputInput" type="text" name="email" value={this.state.email} onChange={this.handleChangeEmail} />
+                        <input className="inputInput" type="text" name="email" value={email}
+                               onChange={this.handleChangeEmail}/>
                     </div>
                     <div className="inputLogin">
                         <label className="inputText">Mot de passe</label>
-                        <input className="inputInput" type="password" name="mdp" value={this.state.motdepasse} onChange={this.handleChangeMdp} />
+                        <input className="inputInput" type="password" name="mdp" value={password}
+                               onChange={this.handleChangeMdp}/>
                     </div>
                     <div className="inputBottom">
-                        <a style={{ position: "absolute", left: 0, cursor: "pointer" }}  onClick={() => setViewId(3)}>Mot de passe oublié ?</a>
-                        <input className="inputSubmit" type="submit" value="Connexion" />
+                        <a style={{position: "absolute", left: 0, cursor: "pointer"}} onClick={() => setViewId(3)}>
+                            Mot de passe oublié ?
+                        </a>
+                        <input className="inputSubmit" value="Connexion" onClick={this.handleSubmit}/>
                     </div>
                 </div>
             </div>
