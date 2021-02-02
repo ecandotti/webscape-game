36118ccb684471566fff1429e6ac4644e48b5e9e
RGUERARD <robin.guerard@ynov.com>
2021-02-02 11:17:52 +0100

    step 1 css

--- src/Views/StepOne.js
+++ src/Views/StepOne.js
@@ -100,8 +100,6 @@ export default class StepOne extends React.Component{
                                 onc de trouver son nom d’
                                 <span className="specialText" onClick={() => this.discoverGoalChar(7) }>u</span>
                                 tilisateur.
-                                <br/> <br/>
-                                Il sait pertinemment que la page [nom d'utilisateur].html lui sera utile.
 
                             </p>
                         </div>
@@ -123,7 +121,7 @@ export default class StepOne extends React.Component{
                     </div>
                     <div className="RightSide">
 
-                            <p className="DescriptionText">
+                            <p className="DescriptionText titre">
                             Biographie de Jean-Michel MICHEL
                         </p>
                         <p className="DescriptionText">
